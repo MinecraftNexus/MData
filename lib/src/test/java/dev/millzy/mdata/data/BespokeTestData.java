94b3e85994fc18f0842522ef805bde76e1408a27
Millzy <millzyyt08@gmail.com>
2023-01-16 22:37:13 +0000

    public testable values

--- lib/src/test/java/dev/millzy/mdata/data/BespokeTestData.java
+++ lib/src/test/java/dev/millzy/mdata/data/BespokeTestData.java
@@ -3,7 +3,7 @@ package dev.millzy.mdata.data;
 import dev.millzy.mdata.Data;
 
 public class BespokeTestData implements Data {
-    String testString = "Test String";
-    int testInt = 10;
-    boolean testBoolean = true;
+    public String testString = "Test String";
+    public int testInt = 10;
+    public boolean testBoolean = true;
 }
