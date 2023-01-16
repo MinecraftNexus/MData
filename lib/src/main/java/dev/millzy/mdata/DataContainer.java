ed83e0454a3d59af3ffc2ea731854c31254cc733
Millzy <millzyyt08@gmail.com>
2023-01-16 10:46:30 +0000

    methods for impl

--- lib/src/main/java/dev/millzy/mdata/DataContainer.java
+++ lib/src/main/java/dev/millzy/mdata/DataContainer.java
@@ -1,5 +1,18 @@
 package dev.millzy.mdata;
 
 public abstract class DataContainer<T> {
+    private T data;
 
+    public abstract void getName();
+
+    public T open() {
+        return data;
+    }
+
+    public void contain(T data) {
+        this.data = data;
+    }
+
+    public abstract void load();
+    public abstract void unload();
 }
