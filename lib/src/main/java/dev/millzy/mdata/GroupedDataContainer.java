16ef3f923d35d63de34771cfaee4e3c74a11d403
Millzy <millzyyt08@gmail.com>
2023-01-16 11:32:32 +0000

    GroupedDataContainer

---
+++ lib/src/main/java/dev/millzy/mdata/GroupedDataContainer.java
@@ -0,0 +1,15 @@
+package dev.millzy.mdata;
+
+import java.util.HashMap;
+
+public abstract class GroupedDataContainer<T> extends DataContainer<HashMap<String, T>> {
+    @Override
+    public void load(MData mData) {
+
+    }
+
+    @Override
+    public void unload(MData mData) {
+
+    }
+}
