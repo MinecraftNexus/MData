ab544b6b925c282e884895032664c07b1f109718
Millzy <millzyyt08@gmail.com>
2023-01-16 11:21:20 +0000

    unload impl

--- lib/src/main/java/dev/millzy/mdata/MData.java
+++ lib/src/main/java/dev/millzy/mdata/MData.java
@@ -1,5 +1,6 @@
 package dev.millzy.mdata;
 
+import java.lang.reflect.InvocationTargetException;
 import java.nio.file.Path;
 
 public class MData {
@@ -21,5 +22,17 @@ public class MData {
         return new MDataFactory();
     }
 
+    public <T extends DataContainer> T loadInto(Class<T> clazz) {
+        try {
+            T container = clazz.getConstructor().newInstance();
+            container.load();
+            return container;
+        } catch (InstantiationException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
+    public void unloadFrom(DataContainer container) {
+        container.unload();
+    }
 }
