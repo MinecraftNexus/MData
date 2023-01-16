c5f8a910b123bd356678ac348a88dd784bab8cec
Millzy <millzyyt08@gmail.com>
2023-01-16 11:24:23 +0000

    Bespoke default impl

---
+++ lib/src/main/java/dev/millzy/mdata/BespokeDataContainer.java
@@ -0,0 +1,13 @@
+package dev.millzy.mdata;
+
+public abstract class BespokeDataContainer<T> extends DataContainer<T> {
+    @Override
+    public void load() {
+
+    }
+
+    @Override
+    public void unload() {
+
+    }
+}
